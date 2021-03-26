03883e447e2611e58606099ef1893adc7abd37ab
normndakbr <normnd.akbr2@gmail.com>
2021-03-26 15:37:49 +0700

    add - app.js

--- server/app.js
+++ server/app.js
@@ -3,7 +3,7 @@ const cors = require("cors");
 const app = express();
 const PORT = 4000;
 const routes = require("./routes/index");
-const errorHandler = require("./middlewares/errorHandler");
+//const errorHandler = require("./middlewares/errorHandler");
 
 app.use(express.urlencoded({extended: true}));
 app.use(cors());
