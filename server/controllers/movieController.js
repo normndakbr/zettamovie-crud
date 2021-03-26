d7fde0e7455665a2729ef88f601563ba121c1081
normndakbr <normnd.akbr2@gmail.com>
2021-03-26 15:28:08 +0700

    add - showAll & showById

--- server/controllers/movieController.js
+++ server/controllers/movieController.js
@@ -1,7 +1,26 @@
 const Movie = require('../models/movie')
 
 class movieController {
-    
+    static async showAll(request, response) {
+        try {
+            const data = await Movie.findAll()
+            response.status(201).json(data)
+        } catch (error) {
+            next(error);
+        }
+    }
+
+    static async showById(request, response) {
+        try {
+			const movieId = +request.params.id;
+			const data = await Movie.findByPk({
+                where: { id: movieId }
+            });
+			response.status(200).json(data);
+		} catch (error) {
+			next(error);
+		}
+    }
 }
 
 module.exports = movieController
